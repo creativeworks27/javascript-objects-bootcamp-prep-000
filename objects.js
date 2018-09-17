cbb73aa1e03de73ad8c3e0653c782e28e8d29559
Roberto Romero <creativeworks27@icloud.com>
2018-09-17 21:45:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playList = { artistnames: "song titles" };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = "songTitle";
+  playlist.artistName = 'songTitle';
   return playlist;
 }
