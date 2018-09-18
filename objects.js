0c2de8093ebedb7867e10e04481a8581a0a01cc8
Roberto Romero <creativeworks27@icloud.com>
2018-09-18 15:09:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,9 @@
-var playList = { artistnames: "song titles" };
-
-function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({playlist}, {artistName : 'songTitle'});
-  return 
+var playlist= {artistnames: `song titles`};
+ function updatePlaylist(playlist,artistName,songTitle){
+  playlist[artistName]= songTitle;
+  return playlist;
+}
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName];
+  return playlist;
 }
